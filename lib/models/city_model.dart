6182b6a8dd1e8dedefb7ca665b3647332e9621d9
leandrorochaadm <leandrorochaadm@gmail.com>
2020-05-21 22:47:29 -0400

    add campo frete_min na city_model

--- lib/models/city_model.dart
+++ lib/models/city_model.dart
@@ -4,24 +4,37 @@ class City {
   double freteNf;
   double redespacho;
   double ajuste;
+  double freteMin;
+  String obs;
 
-  City({this.name, this.freteKg, this.freteNf, this.redespacho, this.ajuste});
+  City(
+      {this.name,
+      this.freteKg,
+      this.freteNf,
+      this.redespacho,
+      this.ajuste,
+      this.freteMin,
+      this.obs});
 
   City.fromMap(Map<String, dynamic> map) {
     name = map['name'];
-    freteKg = map['frete_kg'];
-    freteNf = map['frete_nf'];
+    freteKg = map['freteKg'];
+    freteNf = map['freteNf'];
     redespacho = map['redespacho'];
     ajuste = map['ajuste'];
+    freteMin = map['frete_min'];
+    obs = map['obs'];
   }
 
   Map<String, dynamic> toMap() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['name'] = this.name;
-    data['frete_kg'] = this.freteKg;
-    data['frete_nf'] = this.freteNf;
+    data['freteKg'] = this.freteKg;
+    data['freteNf'] = this.freteNf;
     data['redespacho'] = this.redespacho;
     data['ajuste'] = this.ajuste;
+    data['frete_min'] = this.freteMin;
+    data['obs'] = this.obs;
     return data;
   }
 }
